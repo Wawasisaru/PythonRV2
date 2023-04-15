74a603352a5006c14b50fe46ad730b0cbbd1ea1d
Wawasisaru <nsaruwawasi@gmil,.com>
2023-04-15 17:48:27 +0300

    Arguments & operations

---
+++ Revison/arguments.py
@@ -0,0 +1,20 @@
+# keyword arguments
+# this is where the values are assigned
+#  to the arguments according to there position.
+# or values get assigned to there arguments by their keyword.
+def student(name,age,school):
+    print(f"my name is {name},am{age} old from {school}")
+
+def family(first,second,third):
+    print(f"{first} is is the firstborn,{second} the secondborn and {third}is the lastborn.")
+
+# Positional argments
+# Positional arguments are those arguments where values get assigned to the
+#  arguments by their position when the function is called
+def fruits(sweet,bitter):
+    print(f"I have an {sweet} and {bitter} fruits in my bag")
+
+
+
+
+
---
+++ Revison/math_1.py
@@ -0,0 +1,18 @@
+# Arithmetic operators
+# using the arithmetic operators to perform a function
+# subtractio,addition,division,multiplication
+def addittion (num1,num2):
+    sum = num1 + num2
+    print(sum) 
+# multiplication
+def multiplication(g,h):
+    multiplication = g * h
+    print(multiplication)
+# division
+def division (y,z):
+    division = y / z
+    print(division)
+# reminder
+def reminder (a,b):
+    reminder = a % b
+    print(reminder)
